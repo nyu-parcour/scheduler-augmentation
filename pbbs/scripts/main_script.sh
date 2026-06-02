43b0b56c9bd2c5e9c9ffda7a15a9bb2e6b02846f
Darshan D <darshand2000@gmail.com>
2026-06-02 18:34:54 -0400

    Modify main script

--- pbbs/scripts/main_script.sh
+++ pbbs/scripts/main_script.sh
@@ -19,5 +19,8 @@ nohup python3 -u ./runall -force -cham80 -aug > ./result/nohup_aug.out 2>&1 &
 python3 parse_avg_timing.py > ../result/aug.json
 
 
-python3 generate_pbbs_comparison_table.py unaug.json aug.json
-python3 generate_pbbs_1_40_80_table.py unaug.json aug.json
+python3 generate_pbbs_comparison_table.py unaug.json aug.json > ../result/pbbs_table_all.txt
+python3 generate_pbbs_1_40_80_table.py unaug.json aug.json > ../result/pbbs_table_1_40_80.txt
+
+
+python3 concat_tables.py ../result/pbbs_table_all.txt ../../eval/summary_plots/parlay_table_all.txt ../result/combined_table_all.txt
