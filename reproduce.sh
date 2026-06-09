5d968ab6e2bd8875a18530c6c30971df26aefd50
Sam Westrick <shwestrick@gmail.com>
2026-06-09 11:47:53 -0400

    more gitignore and nicer reproduce output

--- reproduce.sh
+++ reproduce.sh
@@ -3,6 +3,9 @@ set -euo pipefail
 
 cd "$(dirname "$0")"
 
+tmp=$(mktemp)
+trap "rm -f '$tmp'" EXIT
+
 echo "==> Initializing submodules..."
 git submodule update --init --recursive
 
@@ -15,15 +18,18 @@ pbbs/testData/geometryData/randPoints 1000000 pbbs/testData/geometryData/uniform
 
 echo "==> Running original (parallelPlaneSweep)..."
 PARLAY_NUM_THREADS=1 pbbs/benchmarks/rangeQuery2d/parallelPlaneSweep/range \
-    pbbs/testData/geometryData/uniform-square-1M > out
-./extract_last_range.sh out > rq2d_all_diamond
+    pbbs/testData/geometryData/uniform-square-1M > "$tmp"
+./extract_last_range.sh "$tmp" >rq2d_all_diamond
 
 echo "==> Running with grain heuristic (parallelPlaneSweepGrainFix)..."
 PARLAY_NUM_THREADS=1 pbbs/benchmarks/rangeQuery2d/parallelPlaneSweepGrainFix/range \
-    pbbs/testData/geometryData/uniform-square-1M > out
-./extract_last_range.sh out > rq2d_ours_all_diamond
+    pbbs/testData/geometryData/uniform-square-1M > "$tmp"
+./extract_last_range.sh "$tmp" >rq2d_ours_all_diamond
 
 echo "==> Plotting..."
 ./MAKE-PLOTS-FOR-PAPER.sh
 
-echo "==> Done."
+echo "==> Done. Raw data:"
+ls rq2d_all_diamond rq2d_ours_all_diamond
+echo "==> Generated plots:"
+ls scatter_plot_*.pdf
